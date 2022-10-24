59f8feb05705735f0fc03c750ffd1b226916b5ca
Deniree Hernandez <denisa_869@hotmail.com>
2022-10-24 18:15:36 -0400

    Javascript added

---
+++ js/main.js
@@ -0,0 +1,42 @@
+//Algoritmo con condicional y ciclo//
+alert("Welcome to my first Javascript Project");
+
+let yourName = prompt("What is your name?");
+
+//ciclo//
+while (!yourName) {
+    alert("Name cannot be empty");
+
+    yourName = prompt("What is your name?");
+}
+
+let knowMore = prompt(
+    "Hello " + yourName + ", would you like to know more about this new program?"
+);
+
+console.log(yourName);
+console.log(knowMore);
+
+if (knowMore.toLowerCase() == "yes") {
+    alert("Thank you, let me tell you more about this.");
+    let yearBorn = prompt('Please submit the year you were born');
+    let currentYear = new Date().getFullYear(); 
+    console.log(currentYear)
+    let result = (currentYear - yearBorn);
+
+    alert('You are ' + result + ' years old');
+
+    if (result >= 8) {
+        alert('Welcome to pokeapp') 
+    } else {
+        alert('Thank you for coming. Bye')
+    }
+    
+} else {
+    alert("Thank you, hope to see you again");
+}
+
+
+
+
+
