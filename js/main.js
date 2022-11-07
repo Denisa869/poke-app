eaa0ee36042fba3848d59b01e3b49823a690ebb1
Deniree Hernandez <denisa_869@hotmail.com>
2022-11-07 12:11:34 -0400

    added function, constructor, arrays, objects, filter

--- js/main.js
+++ js/main.js
@@ -24,23 +24,49 @@ console.log(knowMore);
 if (knowMore.toLowerCase() == "yes") {
     alert("Thank you, let me tell you more about this.");
     let yearBorn = prompt('Please submit the year you were born');
-    let currentYear = new Date().getFullYear(); 
+    let currentYear = new Date().getFullYear();
     console.log(currentYear)
     let result = (currentYear - yearBorn);
 
     alert('You are ' + result + ' years old');
 
     if (result >= 8) {
-        alert('Welcome to pokeapp') 
+        alert('Welcome to pokeapp')
     } else {
         alert('Thank you for coming. Bye')
     }
-    
+
 } else {
     alert("Thank you, hope to see you again");
-}
-
-
-
-
+} ;
+//Segunda pre-entrega/
+class Pokemon {
+    constructor(name, type, abilities, color, description) {
+      this.name = name;
+      this.type = type;
+      this.abilities = abilities;
+      this.color = color;
+      this.info = function () {
+        return description;
+      };
+    }
+  }
+  const pokemonMudkip = new Pokemon("Mudkip", "Water", "Torrent", "Blue", 'This is a Water-type Pokémon introduced in Generation III. It evolves into Marshtomp starting at level 16, which evolves into Swampert starting at level 36. Mudkip is a small, amphibious, quadrupedal Pokémon. It has a blue body with a light-blue underside.');
+  const pokemonTreecko = new Pokemon("Treecko", "Grass", "Overgrow", "This is a Grass-type Pokémon introduced in Generation III. It evolves into Grovyle starting at level 16, which evolves into Sceptile starting at level 36.");
+  const pokemonTorchic = new Pokemon("Torchic", "Fire", "Blaze", "Red", 'This is a Fire-type Pokémon introduced in Generation III. it evolves into Combusken starting at level 16, which evolves into Blaziken starting at level 36. Torchic is a small, chick Pokémon with stubby, downy, yellow wings. Its body is covered with orange feathers.');
+  
+  
+  const whichPokemon = prompt(
+    "Which pokemon would you like to know more? \n Mudkip \n Treecko \n Torchic"
+  );
+  
+  const pokemon3 = [pokemonMudkip, pokemonTreecko, pokemonTorchic];
+  
+  const result = pokemon3.find((p) => p.name.toLowerCase() === whichPokemon.toLowerCase());
+  
+  if (result) {
+    alert(result.info());
+  } else {
+    alert("Pokémon not found");
+  }
 
