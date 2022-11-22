a996396e742b9f2176835365c280afcc02f54f4b
Deniree Hernandez <denisa_869@hotmail.com>
2022-11-21 21:53:00 -0300

    add conditional

--- js/main.js
+++ js/main.js
@@ -74,43 +74,46 @@ const headers = new Headers();
 headers.set("permissions-policy", "interest-cohort=()");
 
 const formContact = document.querySelector("#form-contact");
-const inlineFormInput = formContact.querySelector("#inlineFormInput");
-const inputEmail = formContact.querySelector("#inputEmail4");
-const inputAddressA = formContact.querySelector("#inputAddress");
-const inputAddressB = formContact.querySelector("#inputAddress2");
-const inputCity = formContact.querySelector("#inputCity");
-const inputZip = formContact.querySelector("#inputZip");
-const newsletterCB = formContact.querySelector("#gridCheck");
 const divContainer = document.querySelector(".main-div-grid-about");
 
 const usuarios = JSON.parse(localStorage.getItem("usuarios")) || [];
 
-formContact.addEventListener("submit", (event) => {
-  event.preventDefault(); //frena el comportamiento por defecto//
+if (formContact) {
+  const inlineFormInput = formContact.querySelector("#inlineFormInput");
+  const inputEmail = formContact.querySelector("#inputEmail4");
+  const inputAddressA = formContact.querySelector("#inputAddress");
+  const inputAddressB = formContact.querySelector("#inputAddress2");
+  const inputCity = formContact.querySelector("#inputCity");
+  const inputZip = formContact.querySelector("#inputZip");
+  const newsletterCB = formContact.querySelector("#gridCheck");
 
-  const userRepeat = usuarios.some((user) => user.email === inputEmail.value);
+  formContact.addEventListener("submit", (event) => {
+    event.preventDefault(); //frena el comportamiento por defecto//
 
-  if (userRepeat) {
-    alert("This email already exist"); //validar que no se repita el email
-    return;
-  }
-  // console.log(userRepeat)
+    const userRepeat = usuarios.some((user) => user.email === inputEmail.value);
 
-  //Crear un objeto con distintas propiedades//
-  const user = {
-    name: inlineFormInput.value,
-    email: inputEmail.value,
-    address1: inputAddressA.value,
-    address2: inputAddressB.value,
-    city: inputCity.value,
-    zip: inputZip.value,
-    newsletter: newsletterCB.checked, // checkbox
-  };
-  usuarios.push(user);
-  localStorage.setItem("usuarios", JSON.stringify(usuarios)); //convertir el objeto en string//
-  formContact.reset(); //Resetea formulario
-  // console.log(user)
-});
+    if (userRepeat) {
+      alert("This email already exist"); //validar que no se repita el email
+      return;
+    }
+    // console.log(userRepeat)
+
+    //Crear un objeto con distintas propiedades//
+    const user = {
+      name: inlineFormInput.value,
+      email: inputEmail.value,
+      address1: inputAddressA.value,
+      address2: inputAddressB.value,
+      city: inputCity.value,
+      zip: inputZip.value,
+      newsletter: newsletterCB.checked, // checkbox
+    };
+    usuarios.push(user);
+    localStorage.setItem("usuarios", JSON.stringify(usuarios)); //convertir el objeto en string//
+    formContact.reset(); //Resetea formulario
+    // console.log(user)
+  });
+}
 
 const newsletter = JSON.parse(localStorage.getItem("usuarios") || []).filter(
   (usuario) => usuario.newsletter
